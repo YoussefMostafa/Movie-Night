a2d55aebb4ba035be2ec81418112908916fe161a
Youssef Mostafa <youssefmostafa403@gmail.com>
2018-10-29 13:22:33 +0200

    Testing Git in xcode

---
+++ Movie Night/Models/TestingGitInXcode.swift
@@ -0,0 +1,13 @@
+//
+//  TestingGitInXcode.swift
+//  Movie Night
+//
+//  Created by Youssef Mostafa on 10/29/18.
+//  Copyright © 2018 UsefDev. All rights reserved.
+//
+
+import Foundation
+
+struct Test {
+    var x = 3
+}
