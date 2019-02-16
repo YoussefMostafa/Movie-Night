2b6cb95faf54006dcda30439eb51029befab938b
Youssef Mostafa <youssefmostafa403@gmail.com>
2019-02-16 12:05:01 +0200

    Added SeeAll button to Home collection headerView.

--- Movie Night/Views/Home/HomeCollectionHeaderView.swift
+++ Movie Night/Views/Home/HomeCollectionHeaderView.swift
@@ -8,24 +8,50 @@
 
 import UIKit
 
-class HomeCollectionHeaderView: UILabel {
+class HomeCollectionHeaderView: UIView {
     
     var type: CollectionType? {
         didSet {
-            text = type?.title
+            typeLabel.text = type?.title
         }
     }
+    
+    var typeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 24, weight: .heavy)
+        return label
+    }()
+    
+    var seeAllButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("See All", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
+        addSubViews()
+        setupConstraints()
     }
     
     private func setupUI() {
-        textColor = .white
-        textAlignment = .left
-        font = UIFont.systemFont(ofSize: 24, weight: .heavy)
         layer.opacity = 9.5
+    }
+    
+    private func addSubViews() {
+        addSubview(typeLabel)
+        addSubview(seeAllButton)
+    }
+    
+    private func setupConstraints() {
+        typeLabel.edgesToSuperView(exclude: .trailing)
+        typeLabel.anchorTrailing(seeAllButton.leadingAnchor, padding: 8)
+        seeAllButton.edgesToSuperView(exclude: .leading)
     }
     
     required init?(coder aDecoder: NSCoder) {
